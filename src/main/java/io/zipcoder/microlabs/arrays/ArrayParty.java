452dd8d297c22e50eeb2be36f8625c39ce5693b2
waxdoll23 <jmcgnty23@gmail.com>
2017-05-11 11:59:52 -0400

    Pushing to update my username for git hub

--- src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
+++ src/main/java/io/zipcoder/microlabs/arrays/ArrayParty.java
@@ -1,10 +1,26 @@
 package io.zipcoder.microlabs.arrays;
 
 
+import java.lang.reflect.Array;
+
 public class ArrayParty {
 
+
     public String printArray(String[] inputArray){
-        return "";
+        String arrayIndexValue = "";
+
+        for(int i = 0; i < inputArray.length; i++ ){
+            if(i > inputArray.length -1) {
+                arrayIndexValue += inputArray[i];
+            }
+            else if(i != inputArray.length){
+                arrayIndexValue += inputArray[i] + "\n";
+            }
+         }
+
+
+       // System.out.print(arrayIndexValue);
+        return "*** Output ***\n" + arrayIndexValue.trim();
     }
 
     public String lastElement(String[] inputArray){
